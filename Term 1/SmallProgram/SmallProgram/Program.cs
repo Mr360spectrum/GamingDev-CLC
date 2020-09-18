2ac837419a89bb4be1595c42ff8619f2d24c3fff
Mr360spectrum <37024501+Mr360spectrum@users.noreply.github.com>
2020-09-18 12:16:44 -0600

    Created SmallProgram

---
+++ Term 1/SmallProgram/SmallProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Karter Ence
+Small Program
+9/18/2020
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am a small program. (Push a key)");
+            Console.ReadKey();
+            Console.WriteLine("Also, Super Mario 3D All-Stars is absolutely amazing. (Push another key)");
+            Console.ReadKey();
+        }
+    }
+}
