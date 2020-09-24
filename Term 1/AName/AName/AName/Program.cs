aa7584ff3f561a80b433befe8c79bf6e1b698d92
Karter Ence <mr360spectrum@gmail.com>
2020-09-24 14:09:08 -0600

    Chapter 3 stuff lol

---
+++ Term 1/AName/AName/AName/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Karter Ence
+A Name
+9/24/2020
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bobolariadociousnapjerdane"; //Declare a variable (also, the s is silent in the name)
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("What is your last name? ");
+            string lastName = Console.ReadLine(); //Get a last name from the user
+            Console.WriteLine("Welcome " + name + " " + lastName + "."); //Concatenate strings
+            Console.ReadKey(); //Wait for input
+        }
+    }
+}
