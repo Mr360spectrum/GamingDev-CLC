67ba23058f1a55c7868b96ec56f0c06ffe30d41e
Karter Ence <mr360spectrum@gmail.com>
2020-09-28 14:02:02 -0600

    Chapter 4 stuff

---
+++ Term 1/Chapter4ExampleKE/Chapter4ExampleKE/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+Karter Ence
+Chapter 4 Example
+9/28/2020
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4ExampleKE
+{
+    class Program
+    {
+        enum FishType
+        {
+            TROUT,
+            BASS,
+            FLOUNDER,
+            CARP,
+            TUNA
+        }
+        static void Main(string[] args)
+        {
+            FishType myFish = FishType.TROUT;
+            Console.WriteLine(myFish);
+
+            //Whole number data types
+            byte byteVar; //1 byte (num up to 255)
+            sbyte sbyteVar; //1 byte (num from -128 to 127)
+            short shortVar; //2 bytes (num from -32,768 to 32,767)
+            int intVar; //4 bytes (num from -2,147,483,648 to 2,147,483,647)
+            uint uintVar; //4 bytes (num up to 4,294,967,295)
+            long longVar; //8 bytes (num from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807)
+
+            byteVar = 17;
+            shortVar = 100;
+            short answer = (short)(byteVar + shortVar);
+            Console.WriteLine(answer);
+
+            //Fractional numbers
+
+            float floatVar; //4 bytes (-3.4 x 10^38 to +3.4 x 10^38 (with 7 significant digits)
+            double doubleVar; //8 bytes (-1.7 x 10^308 to +1.7 x 10^308 (with 15-16 significant digits)
+            decimal decimalVar; //16 bytes (-7.9 x 10^28 to 7.9 x 10^28 (with 28-29 significant digits)
+
+            floatVar = 1.02f;
+            decimalVar = 1.02m;
+            doubleVar = 1.69;
+
+            const double PI = 3.1415926535897932384626;
+
+            int test = (int)doubleVar;
+            Console.WriteLine(test);
+
+            //Non-numeric data
+
+            char charVar; //2 bytes (One text character)
+            bool boolVar; //1 byte (true or false)
+
+            charVar = 'h';
+
+            //int myInt1;
+            //System.Int32 myInt2;
+            //Console.WriteLine(System.Int32.MaxValue);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Term 1/TestScoresKE/TestScoresKE/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Karter Ence
+Test Scores
+9/28/2020
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestScoresKE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare test scores as integers
+            int testScore1 = 85;
+            int testScore2 = 92;
+            int testScore3 = 100;
+            int testScore4 = 80;
+
+            //Print test scores
+            Console.WriteLine("Test score 1: " + testScore1);
+            Console.WriteLine("Test score 2: " + testScore2);
+            Console.WriteLine("Test score 3: " + testScore3);
+            Console.WriteLine("Test score 4: " + testScore4);
+
+            //Find the sum and assign it to the integer "sum"
+            int sum = testScore1 + testScore2 + testScore3 + testScore4;
+            //Find the average and assign it to the integer "average"
+            double average = sum / 4;
+            Console.WriteLine("AVERAGE SCORE = " + average);
+
+            //Wait for input
+            Console.ReadKey();
+        }
+    }
+}
