3e33001d95d00032fa2bdecff63e1548ea7a73ee
Mr360spectrum <37024501+Mr360spectrum@users.noreply.github.com>
2020-10-20 13:21:05 -0600

    Created Loops2

---
+++ Term 1/Loops2/Loops2/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Loops2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runBttn = new System.Windows.Forms.Button();
+            this.outputLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // runBttn
+            // 
+            this.runBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runBttn.Location = new System.Drawing.Point(339, 342);
+            this.runBttn.Name = "runBttn";
+            this.runBttn.Size = new System.Drawing.Size(104, 42);
+            this.runBttn.TabIndex = 0;
+            this.runBttn.Text = "Run";
+            this.runBttn.UseVisualStyleBackColor = true;
+            this.runBttn.Click += new System.EventHandler(this.runBttn_Click);
+            // 
+            // outputLbl
+            // 
+            this.outputLbl.AutoSize = true;
+            this.outputLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLbl.Location = new System.Drawing.Point(317, 79);
+            this.outputLbl.Name = "outputLbl";
+            this.outputLbl.Size = new System.Drawing.Size(152, 55);
+            this.outputLbl.TabIndex = 1;
+            this.outputLbl.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLbl);
+            this.Controls.Add(this.runBttn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runBttn;
+        private System.Windows.Forms.Label outputLbl;
+    }
+}
+
---
+++ Term 1/Loops2/Loops2/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loops2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void runBttn_Click(object sender, EventArgs e)
+        {
+            //int count = 0;
+            //while (count <= 100)
+            //{
+            //    outputLbl.Text = "Count = " + count;
+            //    Console.WriteLine(count);
+            //    MessageBox.Show("Count = " + count);
+            //    count++;
+            //}
+
+            //String target = "Paper or plastic?";
+            //for (int i = 0; i<target.Length; i++)
+            //{
+            //    outputLbl.Text = "count = " + i;
+            //    Console.WriteLine(i);
+            //    MessageBox.Show("Count = " + i);
+            //}
+
+            //for (int dayOfWeek = 0; dayOfWeek < 7; dayOfWeek++)
+            //{
+            //    outputLbl.Text = "Day: " + dayOfWeek;
+            //    for (int hour = 0; hour < 24; hour++)
+            //    {
+            //        MessageBox.Show("Hour: " + hour);
+            //        for (int minute = 0; minute < 60; minute++)
+            //        {
+            //            MessageBox.Show("Minute: " + minute);
+            //            for (int sec = 0; sec < 60; sec++)
+            //            {
+            //                Console.WriteLine(sec);
+            //            }
+            //        }
+            //    }
+            //}
+
+            String text = "Paper or plastic?";
+            foreach (char letter in text)
+            {
+                MessageBox.Show(letter.ToString());
+            }
+        }
+    }
+}
