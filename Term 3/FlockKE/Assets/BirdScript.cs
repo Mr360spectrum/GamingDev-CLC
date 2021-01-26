039d1bb0362ed096febd0fb5f572bc23e82c8ab3
Mr360spectrum <37024501+Mr360spectrum@users.noreply.github.com>
2021-01-25 20:48:39 -0700

    Updated for Reporting for Duty

---
+++ Term 3/FlockKE/Assets/BirdScript.cs
@@ -0,0 +1,24 @@
+﻿/*
+Karter Ence
+Reporting for Duty
+Chapter 3 Activity
+1/25/2021
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(this.gameObject.name + " is reporting for duty!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
