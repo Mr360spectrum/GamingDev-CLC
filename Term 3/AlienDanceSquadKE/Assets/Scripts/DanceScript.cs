79dde62865ba3ce9c6734597df6e018630923b9b
Mr360spectrum <37024501+Mr360spectrum@users.noreply.github.com>
2021-02-01 21:40:24 -0700

    Created Kovarna files and AlienDanceSquad

---
+++ Term 3/AlienDanceSquadKE/Assets/Scripts/DanceScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        transform.Rotate(0.0f, 0.0f, Input.GetAxis("Fire1"));
+        transform.localScale += new Vector3(Input.GetAxis("Fire2"), Input.GetAxis("Fire2"), 0.0f);
+    }
+}
