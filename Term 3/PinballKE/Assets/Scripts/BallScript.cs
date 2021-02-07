52fbefc2e2fbf3fa8ace57310a9398eb28b8587d
Mr360spectrum <37024501+Mr360spectrum@users.noreply.github.com>
2021-02-06 17:19:22 -0700

    Created Activity Projects

---
+++ Term 3/PinballKE/Assets/Scripts/BallScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log(gameObject.name + " collided with " + other.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Game Over!");
+    }
+}
